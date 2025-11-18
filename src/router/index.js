deebf2216e5043ec4635dc8a2d6e849dcc1011a8
jc <pandangoup@outlook.com>
2025-11-18 17:43:45 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+import {getWeb3} from "@/js/web3";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      redirect: '/asset',
-      children: [
-        {
-          path: "/asset",
-          component: () => import("@/views/asset/index.vue"),
-        },
-      ],
+      component: () => import("@/views/index/index.vue"),
     },
   ],
 });
 
 router.beforeEach(async (to, from, next) => {
+  await getWeb3();
   next()
 })
 export default router;
