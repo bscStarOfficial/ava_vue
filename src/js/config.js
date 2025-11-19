0138bb877fb164276377583ce4397d8ef238341a
jc <pandangoup@outlook.com>
2025-11-19 15:59:30 +0800

    contracts

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,11 @@
 const addresses = {
   "56": {
-    staking: '',
-    ava: "",
-    usdt: "0x55d398326f99059fF775485246999027B3197955",
-    multiCall: '',
-    referral: '',
-    rootAddress: '0xEaA5e6c412700F93b232Df91C4Ce5da0C675376C',
+    staking: '0x035bE28C326e1F5A379B668fD2062aE0228D5174',
+    ava: "0x360D14273b684C65C61d097E076Ca449124771BB",
+    usdt: "0xa48B6af4041863e6Ea024C2F33619DDF67cb2E44",
+    multiCall: '0xeB270359Faa618378ad325691F894b52CA50b520',
+    referral: '0x083EBdAB45A4296C032778732543048387208eb3',
+    rootAddress: '0x798967E40fD9fBDDc3F9b985A2EB14c9fDAe1c1F',
   },
   "5611": {
     staking: '0x4057eB6A9416d782F01091F44942c0799c34C782',
